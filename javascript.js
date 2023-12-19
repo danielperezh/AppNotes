edd90f55d85e8b752ede40f64fcb2d5cb888bfa0
Daniel Perez <daniel.perezh@usantoto.edu.co>
2023-12-19 18:40:26 -0500

    Primera version

---
+++ javascript.js
@@ -0,0 +1,43 @@
+let createBox = document.getElementsByClassName('createBox')[0];
+let notes = document.getElementsByClassName('notes')[0];
+let input = document.getElementById('userInput');
+let i = 0;
+
+createBox.addEventListener('keydown', content);
+
+document.getElementById("create").addEventListener("click",
+    function () {
+        createBox.style.display = "block";
+    });
+
+function content(e) {
+    if (e.keyCode == '13') {
+        divstyle(input.value);
+        input.value = "";
+        createBox.style.display = "none";
+    }
+};
+
+function color(){
+    let randomColors = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328"];
+    if(i > randomColors.length - 1){
+        i = 0;
+    }
+    return randomColors[i++];
+}
+
+function divstyle(text) {
+    let div = document.createElement("div");
+    div.className = 'note';
+    div.innerHTML = '<div class="details">'+'<h3>'+text+'</h3></div>';
+
+    //remover barra de borrar
+    div.addEventListener("dblclick", function(){
+        div.remove();
+    });
+
+    //colores aleatoreos de las notas
+    div.setAttribute('style', 'background:'+color()+'');
+
+    notes.appendChild(div);
+};
